e9aa3e52589fee5524676c663e0f7656bec052fb
Kavin-105 <kavin519088@gmail.com>
2026-02-17 23:04:18 +0530

    dish added

--- src/categories/fish.js
+++ src/categories/fish.js
@@ -10,7 +10,7 @@ export const fish = {
     image: fmain,
     items: [
         { id: 1, name: 'Fish Fry(single piece)', price: '₹45', description: 'Fish fry with spices , Onion and lemon', image: regular },
-        { id: 2, name: 'Fish Fry(double piece)', price: '₹80', description: 'Fish fry with spices , Onion and lemon', image: regular, preOrder: true },
+        { id: 2, name: 'Fish Fry(double piece)', price: '₹80', description: 'Fish fry with spices , Onion and lemon', image: regular },
         { id: 3, name: 'Fish Chilli(100gm)', price: '₹50', description: 'Fish chilli 100 grams with onion and lemon', image: chilli },
         { id: 4, name: 'Fish Chilli(250gm)', price: '₹110', description: 'Fish chilli 250 grams with onion and lemon', image: chilli },
         { id: 5, name: 'Jilebi Fish', price: '₹70', description: 'Jilebi fish single piece with onion and lemon', image: jilebi },
