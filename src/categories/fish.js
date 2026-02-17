aafbb65e53bd1ba4ef61bffabcee1372423b70e4
Kavin-105 <kavin519088@gmail.com>
2026-02-18 00:10:52 +0530

    dish added

--- src/categories/fish.js
+++ src/categories/fish.js
@@ -3,6 +3,7 @@ import regular from '../assets/fish/regular.jpg'
 import jilebi from '../assets/fish/jilebi.jpg'
 import chilli from '../assets/fish/Chilli-Fish.jpg'
 import paarai from '../assets/fish/paarai.jpg'
+import meal from '../assets/fish/meal.jpg'
 export const fish = {
     id: 'fish',
     name: 'Fish',
@@ -10,14 +11,14 @@ export const fish = {
     image: fmain,
     items: [
         { id: 1, name: 'Paarai Single Piece', price: '₹45', description: 'Fish fry with spices , Onion and lemon', image: regular },
-        { id: 2, name: 'Paarai Single Piece', price: '₹80', description: 'Fish fry with spices , Onion and lemon', image: regular},
+        { id: 2, name: 'Paarai Double Piece', price: '₹80', description: 'Fish fry with spices , Onion and lemon', image: regular},
         { id: 3, name: 'Chilli 100g', price: '₹50', description: 'Fish chilli 100 grams with onion and lemon', image: chilli },
         { id: 4, name: 'Chilli 250g', price: '₹110', description: 'Fish chilli 250 grams with onion and lemon', image: chilli },
         { id: 5, name: 'Jalebi Full Fish Fry', price: '₹70', description: 'Jilebi fish single piece with onion and lemon', image: jilebi },
-        { id: 6, name: 'Curry Fish Single Piece', price: '₹45', description: 'Jilebi fish single piece with onion and lemon', image: jilebi },
-        { id: 7, name: 'Curry Fish Double Piece', price: '₹80', description: 'Jilebi fish single piece with onion and lemon', image: jilebi },
-        { id: 8, name: 'Curry Fish Triple Piece', price: '₹120', description: 'Paarai fish single piece with onion and lemon', image: paarai },
-        { id: 9, name: 'Fish Meals', price: '₹119', description: 'Paarai fish single piece with onion and lemon', image: paarai },
-        { id: 10, name: 'Fish Meals Parcel', price: '₹129', description: 'Paarai fish double piece with onion and lemon', image: paarai }
+        { id: 6, name: 'Curry Fish 1X', price: '₹45', description: 'Paarai fish single piece with onion and lemon', image: paarai },
+        { id: 7, name: 'Curry Fish 2X', price: '₹80', description: 'Paarai fish Double piece with onion and lemon', image: paarai },
+        { id: 8, name: 'Curry Fish 3X', price: '₹120', description: 'Paarai fish Triple piece with onion and lemon', image: paarai },
+        { id: 9, name: 'Fish Meals', price: '₹119', description: 'Fish meals - white rice ,gravy,Rasam and fish fry 1 pcs', image: meal },
+        // { id: 10, name: 'Fish Meals Parcel', price: '₹129', description: 'Fish meals - white rice ,gravy,Rasam and fish fry 1 pcs', image: meal }
     ]
 };
--- src/categories/kebab.js
+++ src/categories/kebab.js
@@ -15,9 +15,9 @@ export const kebab = {
         { id: 2, name: 'Chicken Mint Kebab', old: '₹120', new: '₹95', description: 'Chicken Mint Kebab 4 pieces in stick', image: mint },
         { id: 3, name: 'Chicken Pineapple Kebab', price: '₹110', description: 'Chicken Pineapple Kebab 4 pieces in stick', image: pineapple },
         { id: 4, name: 'Veg Kebab', price: '₹65', description: 'Veg Kebab 4 pieces in stick', image: veg },
-        { id: 5, name: 'Chicken Kebab with roti', price: '₹95', description: 'Chicken Kebab stuffed in roti with onion , tomato and mayonnaise', image: roti },
-        { id: 6, name: 'Chicken Mint Kebab with roti', price: '₹110', description: 'Chicken Mint Kebab stuffed in roti with onion , tomato and mayonnaise', image: roti },
-        { id: 7, name: 'Chicken Pineapple Kebab with roti', price: '₹130', description: 'Chicken Pineapple Kebab stuffed in roti with onion , tomato and mayonnaise', image: roti },
+        { id: 5, name: 'Chicken Kebab with roti', price: '₹95', description: 'Kebab stuffed in roti with onion , tomato and mayonnaise', image: roti },
+        { id: 6, name: 'Chicken Mint Kebab with roti', price: '₹110', description: 'Kebab stuffed in roti with onion , tomato and mayonnaise', image: roti },
+        { id: 7, name: 'Chicken Pineapple Kebab with roti', price: '₹130', description: 'Kebab stuffed in roti with onion , tomato and mayonnaise', image: roti },
         { id: 8, name: 'Veg Kebab with roti', price: '₹80', description: 'Veg Kebab stuffed in roti with onion , tomato and mayonnaise', image: vegroti }
 
     ]
